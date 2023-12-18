8be54dfe27b2b72585ac191b25fddeb5894b0be8
Relintai <relintai@protonmail.com>
2023-12-19 00:20:47 +0100

    Renamed time.h, and made it a class.

---
+++ sfw/stime.h
@@ -0,0 +1,27 @@
+#ifndef STIME_H
+#define STIME_H
+
+// -----------------------------------------------------------------------------
+// time framework utils
+// - originally by rlyeh, public domain.
+
+#include "int_types.h"
+
+class Time {
+public:
+	static uint64_t date(); // YYYYMMDDhhmmss
+	static uint64_t date_epoch(); // linux epoch
+	static char *date_string(); // "YYYY-MM-DD hh:mm:ss"
+	static double time_hh();
+	static double time_mm();
+	static double time_ss();
+	static uint64_t time_ms();
+	static uint64_t time_us();
+	static uint64_t time_ns();
+	static void sleep_ss(double ss);
+	static void sleep_ms(double ms);
+	static void sleep_us(double us);
+	static void sleep_ns(double us);
+};
+
+#endif
