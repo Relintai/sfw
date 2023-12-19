b36a278f82961c1a83473667790742bd68e46427
Relintai <relintai@protonmail.com>
2023-12-19 15:05:20 +0100

    Create window.

--- game_application.h
+++ game_application.h
@@ -4,11 +4,15 @@
 #include "application.h"
 
 #include "game_scene.h"
+#include "window.h"
 
 class GameApplication : public Application {
 public:
     GameApplication() : Application() {
         scene = new GameScene();
+
+        AppWindow *w = memnew(AppWindow());
+        w->create(100, 0);
     }
 
     ~GameApplication() {
