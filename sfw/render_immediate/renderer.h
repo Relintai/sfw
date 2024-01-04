49d055a9e00237cedff2e127f226b3ee3a494cf0
Relintai <relintai@protonmail.com>
2024-01-04 18:41:16 +0100

    Immediate renderer api setup.

--- sfw/render_immediate/renderer.h
+++ sfw/render_immediate/renderer.h
@@ -3,91 +3,59 @@
 
 #include "core/color.h"
 #include "core/rect2.h"
-#include "render_objects/sprite.h"
-#include "render_core/texture.h"
+#include "core/transform.h"
+#include "core/transform_2d.h"
+#include "core/rect2.h"
+#include "core/vector2i.h"
 
-#include <SDL.h>
+#include "object/object.h"
+#include "object/reference.h"
 
-class Renderer {
+class Mesh;
+class Material;
+class Texture;
+class Font;
+class FontMaterial;
+class TextureMaterial2D;
+
+class Renderer : public Object {
+	SFW_OBJECT(Renderer, Object);
+
 public:
-	void present();
-	void set_draw_color(const Uint8 r, const Uint8 g, const Uint8 b, const Uint8 a);
-	void set_draw_color(const Color &color);
-	void clear();
+	void draw_point(const Vector2 &p_position, const Color &p_color = Color(1, 1, 1));
+	void draw_line(const Vector2 &p_from, const Vector2 &p_to, const Color &p_color = Color(1, 1, 1), const real_t p_width = 1);
+	void draw_line_rect(const Rect2 &p_rect, const Color &p_color = Color(1, 1, 1));
+	void draw_rect(const Rect2 &p_rect, const Color &p_color = Color(1, 1, 1));
 
-	SDL_BlendMode get_blend_mode() const;
-	void set_blend_mode(const SDL_BlendMode mode);
+	void draw_texture(const Ref<Texture> &p_texture, const Rect2 &dst_rect, const Color &p_modulate = Color(1, 1, 1));
+	void draw_texture_clipped(const Ref<Texture> &p_texture, const Rect2 &p_src_rect, const Rect2 &p_dst_rect, const Color &p_modulate = Color(1, 1, 1));
 
-	void draw_point(const int x, const int y);
-	void draw_point(const float x, const float y);
+	void draw_mesh_2d(const Ref<Mesh> &p_mesh, const Ref<Material> &p_material, const Transform2D &p_transform_2d);
+	void draw_mesh_3d(const Ref<Mesh> &p_mesh, const Ref<Material> &p_material, const Transform &p_transform);
 
-	void draw_line(const int x1, const int x2, const int y1, const int y2);
-	void draw_line(const float x1, const float x2, const float y1, const float y2);
+	void draw_text_2d(const Ref<Font> &p_mesh, const Vector2 &p_position, const Color &p_color = Color(1, 1, 1));
+	void draw_text_2d_tf(const Ref<Font> &p_mesh, const Transform2D &p_transform_2d, const Color &p_color = Color(1, 1, 1));
+	void draw_text_2d_tf_material(const Ref<Font> &p_mesh, const Ref<Material> &p_material, const Transform2D &p_transform_2d);
 
-	void draw_rect(const Rect2 &rect);
+	Vector2i get_window_size() const;
+	float get_window_aspect() const;
 
-	void draw_fill_rect(const Rect2 &rect);
-
-	void draw_texture(const Texture &texture, const Rect2 &dst_rect);
-	void draw_texture(const Texture &texture, const Rect2 &src_rect, const Rect2 &dst_rect);
-	void draw_texture(const Texture &texture, const Rect2 &src_rect, const Rect2 &dst_rect, const double angle, const float cx = 0, const float cy = 0, const SDL_RendererFlip flip = SDL_FLIP_NONE);
-
-	void draw_sprite(const Sprite &sprite);
-	void draw_sprite(const Sprite *sprite);
-
-	int get_dpi() const;
-	int get_window_size_w() const;
-	int get_window_size_h() const;
-	int get_size_w() const;
-	int get_size_h() const;
-	void set_size(const int w, const int h) const;
-	void get_size(int *w, int *h) const;
-
-	float get_scale_w() const;
-	float get_scale_h() const;
-	void set_scale(const float w, const float h) const;
-	void get_scale(float *w, float *h) const;
-
-	bool get_integer_scaling() const;
-	void set_integer_scaling(const bool enable);
-
-	Rect2 get_viewport() const;
-	void set_viewport(const Rect2 &rect) const;
-
-	Rect2 get_clip_rect() const;
-	void set_clip_rect(Rect2 *rect) const;
-	bool clip_rect_enabled() const;
-
-	bool render_target_supported();
-
-	SDL_Texture *get_render_target();
-	void set_render_target(Texture *texture);
-
-	void initialize();
-	void destroy();
-
-	SDL_Window *get_window();
-	SDL_Renderer *get_renderer();
+	static void initialize();
+	static void destroy();
 
 	Renderer();
-	Renderer(unsigned int flags, unsigned int window_flags, int window_width = 640, int window_height = 480);
-	virtual ~Renderer();
+	~Renderer();
 
 	static Renderer *get_singleton();
 
 private:
-	int _initial_window_width;
-	int _initial_window_height;
-
-	unsigned int _flags;
-	unsigned int _window_flags;
-
-	SDL_Window *_window;
-	SDL_Renderer *_renderer;
-
-	int _window_display_index;
-
 	static Renderer *_singleton;
+
+	Ref<Mesh> _2d_mesh;
+	Ref<Mesh> _3d_mesh;
+
+	Ref<TextureMaterial2D> _texture_material_2d;
+	Ref<FontMaterial> _font_material;
 };
 
 #endif
