88d03e080abe34aa4895d5d81b94f65fde9dc0b7
Relintai <relintai@protonmail.com>
2024-01-05 22:23:50 +0100

    Renamed the current merge template.

--- tools/merger/join.sh
+++ tools/merger/join.sh
@@ -1,10 +1,11 @@
 
 mkdir -p out
+mkdir -p out/full
 
 cd ..
 cd ..
 
-python tools/merger/join.py --template tools/merger/sfw.h.inl --path . --output tools/merger/out/sfw.h
-python tools/merger/join.py --template tools/merger/sfw.cpp.inl --path . --output tools/merger/out/sfw.cpp
-python tools/merger/join.py --template tools/merger/sfw_3rd.x.inl --path . --output tools/merger/out/sfw_3rd.h
+python tools/merger/join.py --template tools/merger/sfw_full.h.inl --path . --output tools/merger/out/full/sfw.h
+python tools/merger/join.py --template tools/merger/sfw_full.cpp.inl --path . --output tools/merger/out/full/sfw.cpp
+python tools/merger/join.py --template tools/merger/sfw_3rd_full.x.inl --path . --output tools/merger/out/full/sfw_3rd.h
 
