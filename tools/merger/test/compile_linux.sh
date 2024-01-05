a28e434a924dc3190da918fc6fb382f1fd6b80ac
Relintai <relintai@protonmail.com>
2024-01-05 16:51:12 +0100

    Comment.

--- tools/merger/test/compile_linux.sh
+++ tools/merger/test/compile_linux.sh
@@ -2,5 +2,7 @@
 ccache g++ -Wall -g -c sfw.cpp -o sfw.o
 ccache g++ -Wall  -g -c main.cpp -o main.o
 
+#-static-libgcc -static-libstdc++
+
 ccache g++ -Wall -lm -ldl -lpthread -lX11 -g sfw.o main.o -o game 
 
