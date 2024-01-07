4e79c45a6706351b7bfc5ab632d29f0169d7d869
Relintai <relintai@protonmail.com>
2024-01-07 12:16:44 +0100

    Use sfwl core in the doc merger.

--- tools/doc/compile_linux.sh
+++ tools/doc/compile_linux.sh
@@ -1,11 +1,11 @@
 
-cp -u ../merger/out/sfwl_full/sfwl.h sfwl.h
-cp -u ../merger/out/sfwl_full/sfwl.cpp sfwl.cpp
+cp -u ../merger/out/sfwl_core/sfwl.h sfwl.h
+cp -u ../merger/out/sfwl_core/sfwl.cpp sfwl.cpp
 
 ccache g++ -Wall -g -c sfwl.cpp -o sfwl.o
 ccache g++ -Wall -g -c main.cpp -o main.o
 
 #-static-libgcc -static-libstdc++
 
-ccache g++ -Wall -lm -ldl -lpthread -lX11 -g sfwl.o main.o -o game 
+ccache g++ -Wall -lpthread -static-libgcc -static-libstdc++ -g sfwl.o main.o -o game 
 
