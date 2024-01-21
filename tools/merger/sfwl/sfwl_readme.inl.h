626bd62d74a22e3d582c1bfdf030d6f46f845669
Relintai <relintai@protonmail.com>
2024-01-21 11:48:35 +0100

    Rework the merged readme.

---
+++ tools/merger/sfwl/sfwl_readme.inl.h
@@ -0,0 +1,5 @@
+
+// SFW - Simple Framework - Lite Version - https://github.com/Relintai/sfw
+
+// Simple c++ app / game framework inspired by the single file c game engines 
+// and libraries, especially [FWK](https://github.com/r-lyeh/FWK).
