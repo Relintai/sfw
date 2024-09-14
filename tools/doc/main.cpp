b825838a66fd85d22a6bd5f76935ab0435c1406f
Relintai <relintai@protonmail.com>
2024-09-14 20:39:08 +0200

    GUI docs for the full template.

--- tools/doc/main.cpp
+++ tools/doc/main.cpp
@@ -541,12 +541,14 @@ void process_file(const String &path, const String &out_file, const String &temp
 	String compilation_no_renderer = FileAccess::get_file_as_string("compilation_no_renderer.md.html");
 	String compilation_renderer = FileAccess::get_file_as_string("compilation_renderer.md.html");
 	String licenses_renderer = FileAccess::get_file_as_string("licenses_renderer.md.html");
+	String licenses_gui = FileAccess::get_file_as_string("licenses_gui.md.html");
 	String markdeep_min_js = FileAccess::get_file_as_string("markdeep.min.js");
 	String markdeep_theme = FileAccess::get_file_as_string("slate.css");
 
 	index_str = index_str.replace("$FILE_Compilation_No_Renderer$", compilation_no_renderer);
 	index_str = index_str.replace("$FILE_Compilation_Renderer$", compilation_renderer);
 	index_str = index_str.replace("$LICENSES_Renderer$", licenses_renderer);
+	index_str = index_str.replace("$LICENSES_GUI$", licenses_gui);
 
 	index_str = index_str.replace("$MARKDEEP_MIN_JS$", markdeep_min_js);
 	index_str = index_str.replace("$MARKDEEP_THEME$", markdeep_theme);
