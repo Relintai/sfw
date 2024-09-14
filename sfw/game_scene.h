1bb589c3cfa6730b6d73f88f82ea5154d9f8388f
Relintai <relintai@protonmail.com>
2024-09-14 12:24:40 +0200

    Added an anther demo to show how to use tu ui in user code.

--- sfw/game_scene.h
+++ sfw/game_scene.h
@@ -28,7 +28,7 @@ class GameScene : public Scene {
 
 public:
 	enum RenderTypes {
-		RENDER_TYPE_MAX = 15
+		RENDER_TYPE_MAX = 16
 	};
 
 	virtual void input_event(const Ref<InputEvent> &event);
@@ -38,6 +38,7 @@ public:
 	virtual void render_obj();
 	virtual void render_immediate_3d(bool clear_screen = true);
 	virtual void render_gui(bool clear_screen = true);
+	virtual void render_gui_manual(bool clear_screen = true);
 
 	void toggle_thread();
 	static void test_thread_func(void *d);
