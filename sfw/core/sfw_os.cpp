07f3eced6f0f9ce137e93da9ce403908fc1cd000
Relintai <relintai@relintai.net>
2025-11-07 13:35:27 +0100

    Added SFWOS.

---
+++ sfw/core/sfw_os.cpp
@@ -0,0 +1,12 @@
+
+//--STRIP
+#include "sfw_os.h"
+
+#include "core/pool_vector.h"
+#include "core/string_name.h"
+//--STRIP
+
+void SFWOS::setup() {
+}
+
+
---
+++ sfw/core/sfw_os.h
@@ -0,0 +1,13 @@
+//--STRIP
+#ifndef SFW_OS_H
+#define SFW_OS_H
+//--STRIP
+
+class SFWOS {
+public:
+	static void setup();
+};
+
+//--STRIP
+#endif
+//--STRIP
