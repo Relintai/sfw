990a9bec7c5c6409998e2fa18d05be8fadb586fc
Relintai <relintai@relintai.net>
2025-11-07 21:47:14 +0100

    Added missing method for windows.

--- sfw/core/os.cpp
+++ sfw/core/os.cpp
@@ -11,6 +11,31 @@
 #include <unistd.h>
 #endif
 
+#if defined(_WIN64) || defined(_WIN32)
+// From the Pandemonium Engine.
+
+// We need this because GetSystemInfo() is unreliable on WOW64
+// see https://msdn.microsoft.com/en-us/library/windows/desktop/ms724381(v=vs.85).aspx
+// Taken from MSDN
+typedef BOOL(WINAPI *LPFN_ISWOW64PROCESS)(HANDLE, PBOOL);
+LPFN_ISWOW64PROCESS fnIsWow64Process;
+
+BOOL is_wow64() {
+	BOOL wow64 = FALSE;
+
+	fnIsWow64Process = (LPFN_ISWOW64PROCESS)GetProcAddress(GetModuleHandle(TEXT("kernel32")), "IsWow64Process");
+
+	if (fnIsWow64Process) {
+		if (!fnIsWow64Process(GetCurrentProcess(), &wow64)) {
+			wow64 = FALSE;
+		}
+	}
+
+	return wow64;
+}
+#endif
+
+// From the Pandemonium Engine.
 int OS::get_processor_count() {
 #if defined(_WIN64) || defined(_WIN32)
 	SYSTEM_INFO sysinfo;
