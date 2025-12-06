89edfb884b095172e6a2bb07937c6cb1f0c32095
Relintai <relintai@relintai.net>
2025-12-06 12:15:53 +0100

    Test for SFWHash.

--- modules/hash/main.cpp
+++ modules/hash/main.cpp
@@ -6,7 +6,69 @@
 int main(int argc, char **argv) {
 	SFWCore::setup();
 
-	//SFWHash::
+	String test = "ASDFGHJKL";
+
+	SFWHash h;
+
+	ERR_PRINT("MD5");
+	h.init(SFWHash::HASH_MD5);
+	h.hash_string(test);
+	ERR_PRINT(h.get_hash());
+
+	h.reset();
+	h.hash_string(test);
+	h.hash_string(test);
+	ERR_PRINT(h.get_hash());
+
+	ERR_PRINT("SHA1");
+	h.init(SFWHash::HASH_SHA1);
+	h.hash_string(test);
+	ERR_PRINT(h.get_hash());
+
+	h.reset();
+	h.hash_string(test);
+	h.hash_string(test);
+	ERR_PRINT(h.get_hash());
+
+	ERR_PRINT("SHA3_224");
+	h.init(SFWHash::HASH_SHA3_224);
+	h.hash_string(test);
+	ERR_PRINT(h.get_hash());
+
+	h.reset();
+	h.hash_string(test);
+	h.hash_string(test);
+	ERR_PRINT(h.get_hash());
+
+	ERR_PRINT("SHA3_256");
+	h.init(SFWHash::HASH_SHA3_256);
+	h.hash_string(test);
+	ERR_PRINT(h.get_hash());
+
+	h.reset();
+	h.hash_string(test);
+	h.hash_string(test);
+	ERR_PRINT(h.get_hash());
+
+	ERR_PRINT("SHA3_384");
+	h.init(SFWHash::HASH_SHA3_384);
+	h.hash_string(test);
+	ERR_PRINT(h.get_hash());
+
+	h.reset();
+	h.hash_string(test);
+	h.hash_string(test);
+	ERR_PRINT(h.get_hash());
+
+	ERR_PRINT("SHA3_512");
+	h.init(SFWHash::HASH_SHA3_512);
+	h.hash_string(test);
+	ERR_PRINT(h.get_hash());
+
+	h.reset();
+	h.hash_string(test);
+	h.hash_string(test);
+	ERR_PRINT(h.get_hash());
 
 	SFWCore::cleanup();
 
