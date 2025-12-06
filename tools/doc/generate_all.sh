d57c0ec7a3c062246c27e1f67929838bb6cd5d38
Relintai <relintai@relintai.net>
2025-12-06 16:37:32 +0100

    Update the doc generator to use the proper render objects header.

--- tools/doc/generate_all.sh
+++ tools/doc/generate_all.sh
@@ -4,11 +4,12 @@
 ./game ../merger/out/render_core/sfw.h -osfw_render_core.html -tsfw_render_core_template.html
 ./game ../merger/out/render_gui/sfw.h -osfw_render_gui.html -tsfw_render_gui_template.html
 ./game ../merger/out/render_immediate/sfw.h -osfw_render_immediate.html -tsfw_render_immediate_template.html
-./game ../merger/out/render_objects/sfw.h -osfw_render_objects.html -tsfw_render_objects_template.html
 ./game ../merger/out/full/sfw.h -osfw_full.html -tsfw_full_template.html
 
 ./game ../merger/out/sfwl_core/sfwl.h -osfwl_core.html -tsfwl_core_template.html
 ./game ../merger/out/sfwl_full/sfwl.h -osfwl_full.html -tsfwl_full_template.html
 
 ./game ../merger/out/modules/hash/sfw/sfw_hash.h -osfw_hash.html -tsfw_hash_template.html
+
+./game ../merger/out/modules/render_objects/sfw_render_objects.h -osfw_render_objects.html -tsfw_render_objects_template.html
 
