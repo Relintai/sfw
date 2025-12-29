385e15adb0af18d2b81b040435a7454934b65dc4
Relintai <relintai@relintai.net>
2025-12-29 13:26:23 +0100

    Added basic scripts for the audio module.

---
+++ modules/audio/compile_linux.sh
@@ -0,0 +1,12 @@
+
+cp -u ../../tools/merger/out/sfwl_core/sfwl.h sfwl.h
+cp -u ../../tools/merger/out/sfwl_core/sfwl.cpp sfwl.cpp
+
+ccache g++ -Wall -g -c sfwl.cpp -o sfwl.o
+ccache g++ -Wall -g -c sfw_hash.cpp -o sfw_hash.o
+ccache g++ -Wall -g -c main.cpp -o main.o
+
+#-static-libgcc -static-libstdc++
+
+ccache g++ -Wall -lpthread -static-libgcc -static-libstdc++ -g sfwl.o sfw_hash.o main.o -o game 
+
